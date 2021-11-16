f45324efd9db0a902eaa729fa839a2bf4c600044
Keegan Maundrell <gomaun55@gmail.com>
2021-11-15 20:04:09 -0800

    Recipe Fragment Detects Swipe Gestures

--- app/src/main/java/ca/bcit/recipematcher/RecipeDisplayFragment.java
+++ app/src/main/java/ca/bcit/recipematcher/RecipeDisplayFragment.java
@@ -2,12 +2,16 @@ package ca.bcit.recipematcher;
 
 import android.os.Bundle;
 
+import androidx.core.view.MotionEventCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class RecipeDisplayFragment extends Fragment {
@@ -21,8 +25,25 @@ public class RecipeDisplayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe_display, container, false);
+        View view = inflater.inflate(R.layout.fragment_recipe_display, container, false);
 
+        view.setOnTouchListener(new onSwipeTouchListener(view.getContext()) {
+            public void onSwipeTop() {
+                Toast.makeText(view.getContext(), "top", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeRight() {
+                Toast.makeText(view.getContext(), "right", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeLeft() {
+                Toast.makeText(view.getContext(), "left", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeBottom() {
+                Toast.makeText(view.getContext(), "bottom", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        return view;
     }
 
     @Override
@@ -31,7 +52,7 @@ public class RecipeDisplayFragment extends Fragment {
         View v = getView();
         if (v != null) {
             TextView name = v.findViewById(R.id.recipe_name);
-            name.setText("deez nuts");
+            name.setText("recipe name");
         }
     }
 }
