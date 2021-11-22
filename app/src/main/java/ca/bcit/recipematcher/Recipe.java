644d944c3d4e6cf2c3c403102981436270ec7789
Steven Gellert <stevenrgellert@icloud.com>
2021-11-21 21:19:38 -0800

    Upload images and a reference to the uploading user's ID

--- app/src/main/java/ca/bcit/recipematcher/Recipe.java
+++ app/src/main/java/ca/bcit/recipematcher/Recipe.java
@@ -8,15 +8,19 @@ public class Recipe {
     private String category;
     private int rating;
     private List<String> stepList;
+    private String imageURL;
+    private String userUid;
 
     // Empty constructor required by Firestore
     public Recipe() {};
 
-    public Recipe(String recipeName, String ingredients, String category, List<String> stepList) {
+    public Recipe(String recipeName, String ingredients, String category, List<String> stepList, String imageURL, String userUid) {
         this.recipeName = recipeName;
         this.ingredients = ingredients;
         this.category = category;
         this.stepList = stepList;
+        this.imageURL = imageURL;
+        this.userUid = userUid;
         rating = 0;
     }
 
@@ -39,4 +43,8 @@ public class Recipe {
     public List<String> getStepList() {
         return stepList;
     }
+
+    public String getImageURL() { return imageURL; }
+
+    public String getUserUid() { return userUid; }
 }
