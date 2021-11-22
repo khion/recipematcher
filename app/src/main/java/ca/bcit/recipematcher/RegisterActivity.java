8a81e08897179eb92a8f57c45c62785c129dba51
Steven Gellert <stevenrgellert@icloud.com>
2021-11-21 23:16:39 -0800

    Add uploaded recipes to a list in the user's document

--- app/src/main/java/ca/bcit/recipematcher/RegisterActivity.java
+++ app/src/main/java/ca/bcit/recipematcher/RegisterActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,11 +15,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegisterActivity extends AppCompatActivity {
     private static final String USER = "user";
@@ -32,6 +36,7 @@ public class RegisterActivity extends AppCompatActivity {
     private FirebaseDatabase database;
     private DatabaseReference mDatabase;
     private User user;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,6 +100,8 @@ public class RegisterActivity extends AppCompatActivity {
                                     }
                                 }
                             });
+
+                            db.collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getUid()).set(user);
 
 
                         } else {
