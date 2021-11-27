a8a696656683f45100f5773f6df7f924f6717d60
Steven Gellert <stevenrgellert@icloud.com>
2021-11-27 15:47:39 -0800

    Add swiping through recipes to main page

--- app/src/main/java/ca/bcit/recipematcher/RecipeDisplayFragment.java
+++ app/src/main/java/ca/bcit/recipematcher/RecipeDisplayFragment.java
@@ -2,6 +2,7 @@ package ca.bcit.recipematcher;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.core.view.MotionEventCompat;
 import androidx.fragment.app.Fragment;
 
@@ -10,11 +11,27 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class RecipeDisplayFragment extends Fragment {
+
+    FirebaseFirestore db;
 
 
     public RecipeDisplayFragment() {
@@ -27,21 +44,56 @@ public class RecipeDisplayFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_recipe_display, container, false);
 
-        view.setOnTouchListener(new onSwipeTouchListener(view.getContext()) {
-            public void onSwipeTop() {
-                Toast.makeText(view.getContext(), "top", Toast.LENGTH_SHORT).show();
-            }
-            public void onSwipeRight() {
-                Toast.makeText(view.getContext(), "right", Toast.LENGTH_SHORT).show();
-            }
-            public void onSwipeLeft() {
-                Toast.makeText(view.getContext(), "left", Toast.LENGTH_SHORT).show();
-            }
-            public void onSwipeBottom() {
-                Toast.makeText(view.getContext(), "bottom", Toast.LENGTH_SHORT).show();
-            }
+        db = FirebaseFirestore.getInstance();
+        List<String> recipeDocIds = new ArrayList<>();
 
-        });
+        db.collection("recipes")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        for (QueryDocumentSnapshot recipeDocument: task.getResult()) {
+                            recipeDocIds.add(recipeDocument.getId());
+                        }
+                        View v = getView();
+                        displayRecipeInfo(v, recipeDocIds.get(0));
+
+                        view.setOnTouchListener(new onSwipeTouchListener(view.getContext()) {
+                            String recipeDocId = "";
+                            int recipeDocIdsIndex = 0;
+                            public void onSwipeTop() {
+                                Toast.makeText(view.getContext(), "top", Toast.LENGTH_SHORT).show();
+                            }
+                            public void onSwipeRight() {
+                                if (recipeDocIds.size() > recipeDocIdsIndex) {
+                                    recipeDocIdsIndex++;
+                                    if (recipeDocIdsIndex >= recipeDocIds.size()) {
+                                        recipeDocIdsIndex = 0;
+                                    }
+                                    recipeDocId = recipeDocIds.get(recipeDocIdsIndex);
+                                    displayRecipeInfo(v, recipeDocId);
+                                }
+                            }
+                            public void onSwipeLeft() {
+                                if (recipeDocIds.size() > recipeDocIdsIndex) {
+                                    recipeDocIdsIndex--;
+                                    if (recipeDocIdsIndex < 0) {
+                                        recipeDocIdsIndex = recipeDocIds.size() - 1;
+                                    }
+                                    recipeDocId = recipeDocIds.get(recipeDocIdsIndex);
+                                    displayRecipeInfo(v, recipeDocId);
+                                }
+                            }
+                            public void onSwipeBottom() {
+                                Toast.makeText(view.getContext(), "bottom", Toast.LENGTH_SHORT).show();
+                            }
+
+                        });
+                    }
+                });
+
+
+
 
         return view;
     }
@@ -54,5 +106,19 @@ public class RecipeDisplayFragment extends Fragment {
             TextView name = v.findViewById(R.id.recipe_name);
             name.setText("recipe name");
         }
+    }
+
+    private void displayRecipeInfo(View v, String recipeID) {
+        TextView name = v.findViewById(R.id.recipe_name);
+        ImageView image = v.findViewById(R.id.recipe_image);
+        DocumentReference recipeDocRef = db.collection("recipes").document(recipeID);
+        recipeDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Recipe r = documentSnapshot.toObject(Recipe.class);
+                name.setText(r.getRecipeName());
+                Picasso.get().load(r.getImageURL()).into(image);
+            }
+        });
     }
 }
